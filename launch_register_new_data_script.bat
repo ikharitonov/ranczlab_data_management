6e6b04ad171109fa7ac3944e764de06b43519791
ikharitonov <sarfins2101@gmail.com>
2024-02-13 17:07:05 +0100

    name fixes

--- launch_register_new_data_script.bat
+++ launch_register_new_data_script.bat
@@ -1,5 +1,5 @@
-call C:\Users\ONIX\anaconda3\Scripts\activate.bat C:\Users\ONIX\anaconda\envs\mysql
+call C:\Users\ONIX\anaconda3\Scripts\activate.bat C:\Users\ONIX\anaconda3\envs\mysql
 
-call python C:\Users\ONIX\source\ranczlab_data_management\register_new_data_script.python
+call python C:\Users\ONIX\source\ranczlab_data_management\register_new_data_script.py
 
 pause
