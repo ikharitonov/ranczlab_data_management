b9a7f4fe62270abcc02c658ebfa7f5c028e9fd0a
ikharitonov <sarfins2101@gmail.com>
2024-02-13 16:45:57 +0100

    bat script added

---
+++ launch_register_new_data_script.bat
@@ -0,0 +1,5 @@
+call C:\Users\ONIX\anaconda3\Scripts\activate.bat C:\Users\ONIX\anaconda\envs\mysql
+
+call python C:\Users\ONIX\source\ranczlab_data_management\register_new_data_script.python
+
+pause
